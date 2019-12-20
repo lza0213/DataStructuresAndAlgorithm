8bdb014da823d16752995031f3b9cf781554c9e1
lza0213 <1827916843@qq.com>
2019-12-20 11:12:02 +0800

    201911220

--- DataStructuresAndAlgorithm/BuyChicken/Program.cs
+++ DataStructuresAndAlgorithm/BuyChicken/Program.cs
@@ -14,7 +14,7 @@ namespace BuyChicken
                 for (int b = 0; b <= 100; b++)  //b母鸡数量
                     for (int c = 0; c <= 100; c++)  //c小鸡数量
                         if ((a + b + c) == 100 && (a * 5 + b * 3 + c / 3) == 100 && c % 3 == 0)
-                            Console.WriteLine("公鸡{0}只,母鸡,{1}只，小鸡{2}\n", a, b, c);
+                            Console.WriteLine("公鸡{0}只,母鸡,{1}只，小鸡{2}\n", a, b, c);//
             Console.ReadLine();
         }
     }
